b053f9ce3a8f3423cafe120065a27e9c8bd5abfa
Wesley Jennings <140358404+WesJennings@users.noreply.github.com>
2025-07-05 14:19:41 -0500

    Added Pedestal, Growth chamber, and world gen for alexandrite

---
+++ src/main/java/net/exodia/tutorialmod/datagen/ModDatapackEntries.java
@@ -0,0 +1,29 @@
+package net.exodia.tutorialmod.datagen;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+import net.exodia.tutorialmod.TutorialMod;
+import net.exodia.tutorialmod.worldgen.ModBiomeModifiers;
+import net.exodia.tutorialmod.worldgen.ModConfiguredFeatures;
+import net.exodia.tutorialmod.worldgen.ModPlacedFeatures;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModDatapackEntries extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+
+    public ModDatapackEntries(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(TutorialMod.MOD_ID));
+    }
+}
---
+++ src/main/java/net/exodia/tutorialmod/screen/ModMenuTypes.java
@@ -0,0 +1,27 @@
+package net.exodia.tutorialmod.screen;
+
+import net.exodia.tutorialmod.TutorialMod;
+import net.exodia.tutorialmod.screen.custom.GrowthChamberMenu;
+import net.exodia.tutorialmod.screen.custom.PedestalMenu;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModMenuTypes {
+    public static final DeferredRegister<MenuType<?>> MENUS =
+            DeferredRegister.create(Registries.MENU, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<MenuType<PedestalMenu>> PEDESTAL_MENU =
+            MENUS.register("pedestal_menu", () -> IForgeMenuType.create(PedestalMenu::new));
+
+    public static final RegistryObject<MenuType<GrowthChamberMenu>> GROWTH_CHAMBER_MENU =
+            MENUS.register("growth_chamber_menu", () -> IForgeMenuType.create(GrowthChamberMenu::new));
+
+
+    public static void register(IEventBus eventBus) {
+        MENUS.register(eventBus);
+    }
+}
